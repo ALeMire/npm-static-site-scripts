2dd1c249b82cb0724cd0157fb18683f050a6cdcb
Aaron LeMire <aaronlemirerva@gmail.com>
2018-10-25 08:59:18 -0400

    remove project files again

--- project/js/main.js
+++
@@ -1,6 +0,0 @@
-const Window = require('window')
-const whenDomReady = require('when-dom-ready')
-
-const { document } = new Window()
-
-whenDomReady(document).then(() => console.log('DOM is ready yo!'))
